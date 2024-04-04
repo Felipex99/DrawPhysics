e809ba68ced5a983c772023bd471ff39e5ba47b9
felipenunes <felipenuneto@gmail.com>
2024-04-04 17:54:00 -0300

    color pick

--- app/src/main/java/com/example/physics/MainActivity.java
+++ app/src/main/java/com/example/physics/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.skydoves.colorpickerview.ColorPickerDialog;
 
 public class MainActivity extends AppCompatActivity {
     private AppCompatButton config, play, color, pencil, hide;
@@ -12,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         declaracaoComponentes();
+        acaoComponentes();
     }
     public void declaracaoComponentes(){
         config = findViewById(R.id.setting);
@@ -19,5 +23,20 @@ public class MainActivity extends AppCompatActivity {
         color = findViewById(R.id.color);
         pencil = findViewById(R.id.pencil);
         hide = findViewById(R.id.hide);
+    }
+    public void acaoComponentes(){
+        color.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new ColorPickerDialog.Builder(MainActivity.this)
+                        .setTitle("Cor do Pincel")
+                        .setPositiveButton()
+                        .setNegativeButton()
+                        .attachAlphaSlideBar(true)
+                        .attachBrightnessSlideBar(true)
+                        .setBottomSpace(12)
+                        .show();
+            }
+        });
     }
 }
