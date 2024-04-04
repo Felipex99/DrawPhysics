b0186feb7c35da7d829800890c4942b28a4a380b
felipenunes <felipenuneto@gmail.com>
2024-04-04 08:30:11 -0300

    declaracao dos componentes

--- app/src/main/java/com/example/physics/MainActivity.java
+++ app/src/main/java/com/example/physics/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.physics;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private AppCompatButton config, play, color, pencil, hide;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        declaracaoComponentes();
+    }
+    public void declaracaoComponentes(){
+        config = findViewById(R.id.setting);
+        play = findViewById(R.id.play);
+        color = findViewById(R.id.color);
+        pencil = findViewById(R.id.pencil);
+        hide = findViewById(R.id.hide);
     }
 }
